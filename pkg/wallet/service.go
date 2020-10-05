141eb8fed80300ab2467658e8b819b52b1cf84ff
root5427 <shukrik27@gmail.com>
2020-10-05 15:23:42 +0500

    added: errors

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -1,8 +1,54 @@
 package wallet
 
-import "github.com/root5427/wallet/pkg/types"
+import (
+	"errors"
+
+	"github.com/root5427/wallet/pkg/types"
+)
+
+var ErrPhoneRegistered = errors.New("phone already registered")
+var ErrAmountMustBePositive = errors.New("amount must be greater than 0")
+var ErrAccountNotFounf = errors.New("account not found")
 
 type Service struct {
-	accounts []types.Account
-	payments []types.Payment
+	nextAccountID int64
+	accounts      []*types.Account
+	payments      []*types.Payment
+}
+
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, ErrPhoneRegistered
+		}
+	}
+	s.nextAccountID++
+	account := &types.Account{
+		ID:      s.nextAccountID,
+		Phone:   phone,
+		Balance: 0,
+	}
+	s.accounts = append(s.accounts, account)
+
+	return account, nil
+}
+
+func (s *Service) Deposit(accountID int64, amount types.Money) error {
+	if amount <= 0 {
+		return ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+	if account == nil {
+		return ErrAccountNotFounf
+	}
+
+	account.Balance += amount
+	return nil
 }
