75e8c1d0ae5a612121bb8c120177f6b9c4058e25
root5427 <shukrik27@gmail.com>
2020-10-05 20:29:45 +0500

    added: FindPaymentByID

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -1,6 +1,10 @@
 package wallet
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/uuid"
+)
 
 func TestService_FindAccountByID_found(t *testing.T) {
 	svc := &Service{}
@@ -24,5 +28,31 @@ func TestService_FindAccountByID_notFound(t *testing.T) {
 	_, err = svc.FindAccountByID(accountID)
 	if err == nil {
 		t.Errorf("User with ID %v found", accountID)
+	}
+}
+
+func TestService_FindPaymentByID_found(t *testing.T) {
+	svc := &Service{}
+	account, err := svc.RegisterAccount("+992001010000")
+	svc.Deposit(account.ID, 20_000_00)
+	if err != nil {
+		t.Error(err)
+	}
+	payment, err := svc.Pay(account.ID, 10_000_00, "Mobile")
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = svc.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestService_FindPaymentByID_notFound(t *testing.T) {
+	svc := &Service{}
+	paymentID := uuid.New().String()
+	_, err := svc.FindPaymentByID(paymentID)
+	if err == nil {
+		t.Error(err)
 	}
 }
