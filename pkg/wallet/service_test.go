5d05be3a7b304e86c472efe35854ffd6eed2da14
root5427 <shukrik27@gmail.com>
2020-10-06 11:56:30 +0500

    classwork

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -1,14 +1,39 @@
 package wallet
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/root5427/wallet/pkg/types"
 
 	"github.com/google/uuid"
 )
 
+type testService struct {
+	*Service
+}
+
+func newTestService() *testService {
+	return &testService{Service: &Service{}}
+}
+
+func (s *testService) addAccountWithBalance(phone types.Phone, balance types.Money) (*types.Account, error) {
+	account, err := s.RegisterAccount(phone)
+	if err != nil {
+		return nil, fmt.Errorf("can't register account, error = %v", err)
+	}
+
+	err = s.Deposit(account.ID, balance)
+	if err != nil {
+		return nil, fmt.Errorf("can't deposti account, error = %v", err)
+	}
+
+	return account, nil
+}
+
 func TestService_FindAccountByID_found(t *testing.T) {
-	svc := &Service{}
-	account, err := svc.RegisterAccount("+992001010000")
+	svc := newTestService()
+	account, err := svc.addAccountWithBalance("+992001010000", 1)
 	if err != nil {
 		t.Error(err)
 	}
@@ -54,6 +79,29 @@ func TestService_FindPaymentByID_notFound(t *testing.T) {
 	_, err := svc.FindPaymentByID(paymentID)
 	if err == nil {
 		t.Errorf("Found payment by ID: %v", paymentID)
+	}
+}
+
+func TestService_Reject_success(t *testing.T) {
+	svc := newTestService()
+
+	phone := types.Phone("+992001010000")
+	account, err := svc.addAccountWithBalance(phone, 10_000_00)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	payment, err := svc.Pay(account.ID, 1000_0, "auto")
+	if err != nil {
+		t.Errorf("Reject(): can't create payment, error = %v", err)
+		return
+	}
+
+	err = svc.Reject(payment.ID)
+	if err != nil {
+		t.Errorf("Reject(): error = %v", err)
+		return
 	}
 }
 
